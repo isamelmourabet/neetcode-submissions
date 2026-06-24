7958e627a20b0128c184059be0efaf26c0225b0e
Isam Mourabet <74101543+isamelmourabet@users.noreply.github.com>
2026-06-24 23:16:56 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] resultado = new int[temperatures.length];
+
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                resultado[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return resultado;
+    }
+}
